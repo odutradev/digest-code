47021d7f232d5838342bad3ac6b94354a9ee9ad7
odutradev <joao.vitornl@gmail.com>
2025-05-15 17:08:20 -0300

    feat: Adding the validation of gitignore to files

--- src/resources/digest_handler.py
+++ src/resources/digest_handler.py
@@ -8,18 +8,28 @@ class DigestHandler:
     def __init__(self, num_files, directory):
         self.directory = directory
         self.num_files = num_files
+        self.released_files = []
 
     def start(self):
         print(f"Processing up to {self.num_files} files in {self.directory}")
 
-        gitignoreValidation = Gitignore(self.directory)
-        gitignoreValidation.read_gitignore()
+        gitignore_validation = Gitignore(self.directory)
+        gitignore_validation.read_gitignore()
 
         for root, _, files in os.walk(self.directory):
             for file in files:
                 full_path = os.path.join(root, file)
                 hasFile = File.file_exists(full_path)
-                print(full_path, hasFile)
+                if not hasFile: return
+
+                relative_path = os.path.relpath(full_path, start=self.directory)
+                is_ignored = gitignore_validation.is_ignored(relative_path)
+
+
+                print(relative_path, is_ignored)
+
+
+                
 
 
 
